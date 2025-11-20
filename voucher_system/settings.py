6a05ff3f41edb3fa52a1001967d92a517850ccca
DarcoDaring <abinbbabu100@gmail.com>
2025-11-20 13:09:48 +0530

    tt

--- voucher_system/settings.py
+++ voucher_system/settings.py
@@ -77,10 +77,9 @@ WSGI_APPLICATION = 'voucher_system.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}')
+    )
 }
 
 
