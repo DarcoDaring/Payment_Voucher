9466a35d5bf7400be4fea7d2b6adc7cad0bfba5b
DarcoDaring <abinbbabu100@gmail.com>
2025-12-06 14:15:26 +0530

    tt

--- voucher_system/build.sh
+++ voucher_system/build.sh
@@ -6,12 +6,12 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Create superuser automatically if it doesn't exist
-python manage.py shell << EOF
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
-    print('Superuser created successfully!')
-else:
-    print('Superuser already exists')
-EOF
+#python manage.py shell << EOF
+#from django.contrib.auth import get_user_model
+#User = get_user_model()
+#if not User.objects.filter(username='admin').exists():
+ #   User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+  #  print('Superuser created successfully!')
+#else:
+#    print('Superuser already exists')
+#EOF
